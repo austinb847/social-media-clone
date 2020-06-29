a343dbff4c8a3316a09f8741b08d658d0fd4c637
Daniel Merys <dkmerys@gmail.com>
2020-06-29 11:43:35 -0700

    add Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Search from "./Search";
+import Tweet from "./Tweet";
 
 function Header(){
   return (
     <React.Fragment>
-
+      <NavBar />
+      <Search />
+      <Tweet />
     </React.Fragment>
   );
 }
+
+export default Header;
