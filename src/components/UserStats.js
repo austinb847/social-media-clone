df8a7ba39fadef9261fbae77c13a3c711ede9d5d
Daniel Merys <dkmerys@gmail.com>
2020-06-29 11:46:37 -0700

    add UserStats component

---
+++ src/components/UserStats.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UserStats(){
+  return(
+    <h3>Kellie A. Corrigan</h3>
+    <p>Tweets</p>
+    <p>Following</p>
+    <p>Followers</p>
+  );
+}
+
+export default UserStats;
