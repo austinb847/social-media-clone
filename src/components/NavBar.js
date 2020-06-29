14a77306dd166b5a8329ad989be3f85cd9fe095e
Daniel Merys <dkmerys@gmail.com>
2020-06-29 13:08:54 -0700

    add NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   return (
     <ul id="nav-bar">
-      
-    </ul
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Notifications</a></li>
+      <li><a href="#">Messages</a></li>
+    </ul>
   );
 }
+
+export default NavBar;
