c473cb6771b9511184ab770c82ea2751e1450b91
Daniel Merys <dkmerys@gmail.com>
2020-06-29 11:52:54 -0700

    add Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 function Search(){
-  
+  return (
+    <form>
+      <input type="text" name="search" placeholder="search" />
+      <input type="submit" value="submit" />
+    </form>
+  );
 }
+
+export default Search;
